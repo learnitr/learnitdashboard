b9e59795271527e6022da5667497913f6ee6160d
MartinLecocq <Martinl@hotmail.be>
2022-05-18 16:53:09 +0200

    Modification de la structure du script. J'ai également réussis à faire en sorte que les inputs de dates apparaissent conditionnelement, et que cela n'engendre pas de bug pour l'évènement réactif qui crée la requête. J'ai réussi cela en créant un input caché via shinyjs dans le uiOutput.

--- R/mod_sdd_tables.R
+++ R/mod_sdd_tables.R
@@ -109,22 +109,100 @@ mod_sdd_tables_server <- function(id){
       logins <- NULL
     }
 
-# Reactive Values ---------------------------------------------------------
+
+# Selectors ---------------------------------------------------------------
+
     
-    dates_changed <- reactiveVal(0)
-    observeEvent({
-      input$sdd_selected_date1
-      input$sdd_selected_date2
-    }, {
-      dates_changed(dates_changed()+1)
+    # Display // DT cols selector
+    output$dt_cols_selector <- renderUI({
+      req(input$activetab)
+      
+      # Getting the right table depending on the tab
+      table <- switch (input$activetab,
+                       "H5P" = h5p(),
+                       "Learnr" = learnr(),
+                       "Shiny" = shiny()
+      )
+      
+      # If there was no error while loading the table
+      if (!inherits(table, "try-error")) {
+        tagList(
+          tags$h3("Columns :"),
+          # Creation of the selector of cols to show, because the page is too small for everything
+          selectInput(ns("dt_selected_cols"), NULL, choices = c("All",names(table)), multiple = TRUE, selected = "All")
+        )
+      } else { NULL }
     })
+    
+    # Display // Login selector
+    output$sdd_login_selector <- renderUI({
+      # If there was no error while getting the logins
+      if (!is.null(logins)) {
+        tagList(
+          tags$h3("Login :"),
+          # Creation of selector with choices "All" and the logins
+          selectInput(ns("sdd_selected_login"), NULL, choices = c("All", logins), selected = "All")
+        )
+      } else { NULL }
+    })
+    
+    # Display // Dates selectors
+    output$sdd_dates_selectors <- renderUI({
+      if (input$is_dates == TRUE) {
+        tagList(
+          fluidRow(
+            # First selector of date
+            column(width = 3,
+                   tags$h3("From :"),
+                   dateInput(ns("sdd_selected_date1"), NULL, value = input$sdd_selected_date1)
+            ),
+            # Second selector of date
+            column(width = 3,
+                   tags$h3("To :"),
+                   dateInput(ns("sdd_selected_date2"), NULL, value = input$sdd_selected_date2)
+            ),
+          )
+        )
+      } else {
+        tagList(
+          shinyjs::hidden(dateInput(ns("sdd_selected_date1"), NULL, value = input$sdd_selected_date1)),
+          shinyjs::hidden(dateInput(ns("sdd_selected_date2"), NULL, value = input$sdd_selected_date2)),
+        )
+      }
+    })
+    
+    # Display // App selector
+    output$sdd_app_selector <- renderUI({
+      req(input$activetab)
+      
+      # Getting the right table
+      table <- switch (input$activetab,
+                       "H5P" = sdd_h5p,
+                       "Learnr" = sdd_learnr,
+                       "Shiny" = sdd_shiny
+      )
+      
+      # Getting table's apps
+      table_apps <- try(sort(table$distinct("app")), silent = TRUE)
+      
+      # Displaying the selector if table_apps didn't occur error
+      if (!inherits(table_apps, "try-error")) {
+        tagList(
+          tags$h3("Application :"),
+          selectInput(ns("sdd_selected_app"), NULL, choices = c("All", table_apps), selected = "All")
+        )
+      }
+    })
+    
+# Request ---------------------------------------------------------
     
     # Variable : Definition of the request, "All" or only one selected login
     request <- eventReactive({
-      input$sdd_selected_login
-      input$sdd_selected_app
-      input$is_dates
-      dates_changed()
+      print(input$sdd_selected_login)
+      print(input$sdd_selected_app)
+      print(input$is_dates)
+      print(input$sdd_selected_date1)
+      print(input$sdd_selected_date2)
       }, {
       # Is there a login request ?
       login_request <- !is.null(input$sdd_selected_login) && input$sdd_selected_login != "All"
@@ -165,90 +243,13 @@ mod_sdd_tables_server <- function(id){
     
     # Defining tables depending on the request
     observeEvent(request(), {
+      print(request())
       {message("requete h5p");h5p(try(sdd_h5p$find(request(), limit = 1000), silent = TRUE))}
       {message("requete learnr");learnr(try(sdd_learnr$find(request(), limit = 1000), silent = TRUE))}
       {message("requete shiny");shiny(try(sdd_shiny$find(request(), limit = 1000), silent = TRUE))}
     })
     
 # DT Displays -------------------------------------------------------------
-    
-    # Display // DT cols selector
-    output$dt_cols_selector <- renderUI({
-      req(input$activetab)
-      
-      # Getting the right table depending on the tab
-      table <- switch (input$activetab,
-                       "H5P" = h5p(),
-                       "Learnr" = learnr(),
-                       "Shiny" = shiny()
-      )
-      
-      # If there was no error while loading the table
-      if (!inherits(table, "try-error")) {
-        tagList(
-          tags$h3("Columns :"),
-          # Creation of the selector of cols to show, because the page is too small for everything
-          selectInput(ns("dt_selected_cols"), NULL, choices = c("All",names(table)), multiple = TRUE, selected = "All")
-        )
-      } else { NULL }
-    })
-    
-    # Display // Login selector
-    output$sdd_login_selector <- renderUI({
-      # If there was no error while getting the logins
-      if (!is.null(logins)) {
-        tagList(
-          tags$h3("Login :"),
-          # Creation of selector with choices "All" and the logins
-          selectInput(ns("sdd_selected_login"), NULL, choices = c("All", logins), selected = "All")
-        )
-      } else { NULL }
-    })
-    
-    # Display // Dates selectors
-    output$sdd_dates_selectors <- renderUI({
-      if (input$is_dates == TRUE) {
-      tagList(
-        fluidRow(
-          # First selector of date
-          column(width = 3,
-            tags$h3("From :"),
-            dateInput(ns("sdd_selected_date1"), NULL)
-          ),
-          # Second selector of date
-          column(width = 3,
-            tags$h3("To :"),
-            dateInput(ns("sdd_selected_date2"), NULL)
-          ),
-        )
-      )
-      } else {
-        NULL
-      }
-    })
-    
-    # Display // App selector
-    output$sdd_app_selector <- renderUI({
-      req(input$activetab)
-      
-      # Getting the right table
-      table <- switch (input$activetab,
-                       "H5P" = sdd_h5p,
-                       "Learnr" = sdd_learnr,
-                       "Shiny" = sdd_shiny
-      )
-      
-      # Getting table's apps
-      table_apps <- try(sort(table$distinct("app")), silent = TRUE)
-      
-      # Displaying the selector if table_apps didn't occur error
-      if (!inherits(table_apps, "try-error")) {
-        tagList(
-          tags$h3("Application :"),
-          selectInput(ns("sdd_selected_app"), NULL, choices = c("All", table_apps), selected = "All")
-        )
-      }
-    })
     
     # Display // H5P datatable
     output$sdd_h5p_dt <- renderDT({
