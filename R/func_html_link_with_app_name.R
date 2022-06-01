e10dc9f4aa1c04864798c983149ecd98ebcba604
MartinLecocq <65352104+MartinLecocq@users.noreply.github.com>
2022-06-01 15:40:52 +0200

    Modification de la fonction qui génère le content, pour n'avoir qu'un petit + avec l'url alternative.

--- R/func_html_link_with_app_name.R
+++ R/func_html_link_with_app_name.R
@@ -1,4 +1,6 @@
 #' HTML Link with App Names
+#' 
+#' Description
 #'
 #' @param data_frame data.frame from which it takes the url and app.
 #'
@@ -9,6 +11,6 @@
 #' # Need special data frame
 html_link_with_app_name <- function(data_frame) {
   if (!any(!c("alt_url", "url", "app") %in% names(data_frame))) {
-    return(glue::glue(r"--[<a href="<<data_frame$url>>"><<data_frame$app>></a><br><a href="<<data_frame$alt_url>>">Alternative URL</a>]--", .open = "<<", .close = ">>"))
+    return(glue::glue(r"--[<a href="<<data_frame$url>>"><<data_frame$app>></a> (<a href="<<data_frame$alt_url>>">+</a>)]--", .open = "<<", .close = ">>"))
   }
 }
