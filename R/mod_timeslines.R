41425cfcd1b736cdf42977df73d039b19e8a0f40
MartinLecocq <65352104+MartinLecocq@users.noreply.github.com>
2022-06-03 15:32:06 +0200

    Mise en place d'une nouvelle timeline qui ne reprend que le planning et qui a également des sélecteurs pour voyager à travers celle-ci. Sélecteur de label à la place d'un sélecteur d'app.

--- R/mod_timeslines.R
+++ R/mod_timeslines.R
@@ -11,8 +11,11 @@ mod_timeslines_ui <- function(id){
   ns <- NS(id)
   tagList(
     
-    # UIoutput for the timeline
+    # UIoutput for the apps timeline
     uiOutput(ns("ui_apps_timeline")),
+    
+    # UIoutput for the planning timeline
+    uiOutput(ns("ui_planning_timeline"))
   )
 }
     
@@ -50,11 +53,10 @@ mod_timeslines_server <- function(id, all_vars){
               tagList(
                 tags$br(),
                 tags$h4("Navigation :"),
-                actionButton(ns("center_to_actual_week"), "Center to current Week"),
+                actionButton(ns("apps_center_to_actual_week"), "Center to current Week"),
                 tags$br(),
                 tags$br(),
-                selectInput(ns("center_to_month"), NULL, choices = c("Center to Month","September", "October", "November", "December", "January", "February", "March", "April", "May", "June", "July", "August"), width = "90%"),
-                tags$h4("App :"),
+                selectInput(ns("apps_center_to_month"), NULL, choices = c("Center to Month","September", "October", "November", "December", "January", "February", "March", "April", "May", "June", "July", "August"), width = "90%"),
                 selectInput(ns("selected_app"), NULL, choices = c("Center to App", sdd_apps$distinct("app")), width = "90%")
               )
             ),
@@ -70,7 +72,7 @@ mod_timeslines_server <- function(id, all_vars){
     # Variable : Data of the timeline
     timeline_data <- NULL
     apps_data <- try(na.omit(sdd_apps$find('{}', fields = '{"app" : true, "start" : true ,"end" : true, "icourse" : true, "type" : true, "url" : true, "alt_url" : true}')), silent = TRUE)
-    planning_data <- try(na.omit(sdd_planning$find('{}', fields = '{"label" : true, "start" : true, "end" : true, "url" : true, "alt_url" : true, "summary" : true}')), silent = TRUE)
+    planning_data <- try(na.omit(sdd_planning$find('{}', fields = '{"label" : true, "start" : true, "end" : true, "url" : true, "alt_url" : true, "summary" : true, "icourse" : true}')), silent = TRUE)
     
     # Preparing the timeline_data
     if (!inherits(apps_data, "try-error") && !inherits(planning_data, "try-error")) {
@@ -121,9 +123,81 @@ mod_timeslines_server <- function(id, all_vars){
       }
     })
 
+# Planning Timeline -------------------------------------------------------
+
+    # Display the output inside the UI if there is no error to get the apps databases
+    output$ui_planning_timeline <- renderUI({
+      if (!inherits(sdd_planning, "try-error")) {
+        tagList(
+          # Box in which the timeline will appear
+          box( title = "Planning Timeline (grouped by icourse)", solidHeader = TRUE,
+               width = 12, icon = shiny::icon("calendar", verify_fa = FALSE),
+               collapsible = TRUE, label = boxLabel(3, "danger"), status = "purple",
+               # Sidebar for timeline options
+               sidebar = boxSidebar(
+                 id = ns("planning_timeline_sb"),
+                 width = 25,
+                 tagList(
+                   tags$br(),
+                   tags$h4("Navigation :"),
+                   actionButton(ns("plan_center_to_actual_week"), "Center to current Week"),
+                   tags$br(),
+                   tags$br(),
+                   selectInput(ns("plan_center_to_month"), NULL, choices = c("Center to Month","September", "October", "November", "December", "January", "February", "March", "April", "May", "June", "July", "August"), width = "90%"),
+                   selectInput(ns("selected_label"), NULL, choices = c("Center to Label", sdd_planning$distinct("label")), width = "90%")
+                 )
+               ),
+               timevisOutput(ns("planning_timeline"))
+          )
+        )
+      }
+    })
+    
+    # Updating the boxsidebar to make it available
+    updateBoxSidebar("planning_timeline_sb", session = session)
+    
+    # Variable : Data of the timeline
+    planning_timeline_data <- NULL
+    # Getting the planning data
+    planning_timeline_data <- try(na.omit(sdd_planning$find('{}', fields = '{"label" : true, "start" : true, "end" : true, "url" : true, "alt_url" : true, "summary" : true, "icourse" : true}')), silent = TRUE)
+    
+    if (!inherits(planning_timeline_data, "try-error")) {
+      # Preparing the content for the timeline
+      planning_timeline_data$content <- try(prepare_content(planning_timeline_data))
+      
+      if (!is.null(planning_timeline_data$content)) {
+        # Taking the good columns
+        planning_timeline_data <- planning_timeline_data[c("_id", "content", "icourse", "start", "end", "summary", "label")]
+        # Setting the good names for columns for timevis
+        names(planning_timeline_data) <- c("id", "content", "group", "start", "end", "title", "label")
+        # Getting the groups
+        attr(planning_timeline_data, "groups") <- data.frame(
+          id = unique(planning_timeline_data$group),
+          content = unique(planning_timeline_data$group)
+        )
+      } else { planning_timeline_data <- NULL }
+    } else { planning_timeline_data <- NULL }
+    
+    # Display the timeline when the output is available and when the data is available
+    output$planning_timeline <- renderTimevis({
+      if (!is.null(planning_timeline_data)) {
+        # Creation of start date for the initial window of the timeline
+        start <- lubridate::floor_date(Sys.time(), "week")
+        lubridate::day(start) <- lubridate::day(start) + 1
+        # Creation of end date for the initial window of the timeline
+        end <- lubridate::ceiling_date(Sys.time(), "week")
+        lubridate::day(end) <- lubridate::day(end) + 1
+        config <- list(
+          start = start,
+          end = end
+        )
+        return(timevis(planning_timeline_data, groups = attr(planning_timeline_data, "groups"), options = config))
+      }
+    })
+    
 # Timeline Update ---------------------------------------------------------
 
-    # Update - Changing the timevis window
+    # Update Apps - Changing the timevis window
     observeEvent(input$selected_app, {
       if (input$selected_app != "Center to App") {
         # Get the dates of start and end of selected app
@@ -137,8 +211,22 @@ mod_timeslines_server <- function(id, all_vars){
       }
     })
     
-    # Update - Getting back to the current week
-    observeEvent(input$center_to_actual_week, {
+    # Update Planning - Changing the timevis window
+    observeEvent(input$selected_label, {
+      if (input$selected_label != "Center to Label") {
+        # Get the dates of start and end of selected label
+        start <- as.POSIXct(na.omit(planning_timeline_data[planning_timeline_data$label == input$selected_label,"start"]))
+        end <- as.POSIXct(na.omit(planning_timeline_data[planning_timeline_data$label == input$selected_label,"end"]))
+        # Change dates to have a range the day to the next
+        start <- lubridate::floor_date(start, "day")
+        lubridate::day(end) <- lubridate::day(end) + 1
+        # Set the window on the range of dates
+        setWindow("planning_timeline", start = start, end = end)
+      }
+    })
+    
+    # Update Apps - Getting back to the current week
+    observeEvent(input$apps_center_to_actual_week, {
       # Creation of start date of the week
       start <- lubridate::floor_date(Sys.time(), "week")
       lubridate::day(start) <- lubridate::day(start) + 1
@@ -148,13 +236,24 @@ mod_timeslines_server <- function(id, all_vars){
       setWindow("apps_timeline", start = start, end = end)
     })
     
-    # Update - Getting back to the current week
-    observeEvent(input$center_to_month, {
-      if (input$center_to_month != "Center to Month") {
+    # Update Planning - Getting back to the current week 
+    observeEvent(input$plan_center_to_actual_week, {
+      # Creation of start date of the week
+      start <- lubridate::floor_date(Sys.time(), "week")
+      lubridate::day(start) <- lubridate::day(start) + 1
+      # Creation of end date of the week
+      end <- lubridate::ceiling_date(Sys.time(), "week")
+      lubridate::day(end) <- lubridate::day(end) + 1
+      setWindow("planning_timeline", start = start, end = end)
+    })
+    
+    # Update Apps - Getting back to the current week
+    observeEvent(input$apps_center_to_month, {
+      if (input$apps_center_to_month != "Center to Month") {
         # Getting current time
         time <- Sys.Date()
         # Getting the month
-        sel_month <- switch (input$center_to_month,
+        sel_month <- switch (input$apps_center_to_month,
           "September" = 9,
           "October" = 10,
           "November" = 11,
@@ -178,6 +277,39 @@ mod_timeslines_server <- function(id, all_vars){
         start <- lubridate::floor_date(time, "month")
         end <- lubridate::ceiling_date(time, "month") - 1
         setWindow("apps_timeline", start = start, end = end)
+      }
+    })
+    
+    # Update Planning - Getting back to the current week
+    observeEvent(input$plan_center_to_month, {
+      if (input$plan_center_to_month != "Center to Month") {
+        # Getting current time
+        time <- Sys.Date()
+        # Getting the month
+        sel_month <- switch (input$plan_center_to_month,
+                             "September" = 9,
+                             "October" = 10,
+                             "November" = 11,
+                             "December" = 12,
+                             "January" = 1,
+                             "Februrary" = 2,
+                             "March" = 3,
+                             "April" = 4,
+                             "May" = 5,
+                             "June" = 6,
+                             "July" = 7,
+                             "August" = 8
+        )
+        # Setting the good year
+        if (sel_month > 8) {
+          lubridate::year(time) <- lubridate::year(time) - 1
+        }
+        # Setting the selected month
+        lubridate::month(time) <- sel_month
+        # Getting start day of month and end day
+        start <- lubridate::floor_date(time, "month")
+        end <- lubridate::ceiling_date(time, "month") - 1
+        setWindow("planning_timeline", start = start, end = end)
       }
     })
     
