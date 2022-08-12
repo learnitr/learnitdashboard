150d78345d39ae184d07ede6052e0dc3c8cb3ae2
MartinLecocq <65352104+MartinLecocq@users.noreply.github.com>
2022-08-12 14:32:12 +0200

    Spelling errors.

--- R/func_prepare_url_alt_url.R
+++ R/func_prepare_url_alt_url.R
@@ -13,36 +13,36 @@ prepare_url_alr_url <- function(data_table) {
   # If url and alt_url and template
   if (!any(!c("url", "alt_url","template") %in% names(data_table))) {
     
-    # Preparinf the url in html link
+    # Preparing the url in html link
     data_table$url <- paste0('<a href="',data_table$url,'" target="_blank">',data_table$url,'</a>')
-    # Preparinf the alt_url in html link
+    # Preparing the alt_url in html link
     data_table$alt_url <- paste0('<a href="',data_table$alt_url,'" target="_blank">',data_table$alt_url,'</a>')
-    # Preparinf the template in html link
+    # Preparing the template in html link
     data_table$template <- paste0('<a href="',data_table$template,'" target="_blank">',data_table$template,'</a>')
     
     return(data_table)
   # If url and alt_url
   } else if (!any(!c("url", "alt_url") %in% names(data_table))) {
     
-    # Preparinf the url in html link
+    # Preparing the url in html link
     data_table$url <- paste0('<a href="',data_table$url,'" target="_blank">',data_table$url,'</a>')
-    # Preparinf the alt_url in html link
+    # Preparing the alt_url in html link
     data_table$alt_url <- paste0('<a href="',data_table$alt_url,'" target="_blank">',data_table$alt_url,'</a>')
     
     return(data_table)
   # If only url and avatar
   } else if (!any(!c("url", "avatar") %in% names(data_table))) {
   
-    # Preparinf the url in html link
+    # Preparing the url in html link
     data_table$url <- paste0('<a href="',data_table$url,'" target="_blank">',data_table$url,'</a>')
-    # Preparinf the alt_url in html link
+    # Preparing the alt_url in html link
     data_table$avatar <- paste0('<a href="',data_table$avatar,'" target="_blank">',data_table$avatar,'</a>')
     
     return(data_table)
   # If only url
   } else if ("url" %in% names(data_table)) {
     
-    # Preparinf the url in html link
+    # Preparing the url in html link
     data_table$url <- paste0('<a href="',data_table$url,'" target="_blank">',data_table$url,'</a>')
     
     return(data_table)
